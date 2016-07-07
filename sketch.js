389c5babf4260b466d424ae793282cb369c7e9db
Jaime Patarroyo <j@jaimepatarroyo.com>
2016-07-07 00:25:00 -0400

    Touch implementation

--- sketch.js
+++ sketch.js
@@ -5,20 +5,20 @@ function setup() {
 function draw() {
   noStroke();
   if (mouseIsPressed) {
-    stroke(0);
     fill(0);
   } else {
     fill(255);
   }
   ellipse(mouseX,mouseY,50,50);
-  ellipse(touchX,touchY,50,50);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function touchMoved() {
-  // do some stuff
+function touchStarted() {
+  fill(0);
+  ellipse(touchX, touchY, 50, 50);
+  // prevent default
   return false;
 }
